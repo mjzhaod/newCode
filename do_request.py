9e91b029dae04d81cafe8e9535ea0c9906528435
greenabyss <weichunlong@migu.cn>
2021-04-20 15:35:16 +0800

    把token分离出来了

--- do_request.py
+++ do_request.py
@@ -14,7 +14,7 @@ def do_request(request):
     if method== 'get':
         response=request.get(url)
     if method== 'post':
-        if json.loads(headers)!=None:
+        if json.loads(headers) is not None:
             response=requests.post(url=url,headers=json.loads(headers),data=json.dumps(json.loads(body)))
         else:
             response=requests.post(url=url,data=json.dumps(json.loads(body)))
@@ -27,3 +27,15 @@ def build_url(url,url_param):
     else:
         url=url+'&authoToken='+url_param
     return url
+
+
+def check(request):
+    result=do_request(request)
+    if result.response_status==200:
+        return result.text
+    elif (result.status_code == 401):
+        raise Exception("用户没有登陆")
+    else:
+        raise Exception("服务器错误" + str(result.status_code))
+
+
